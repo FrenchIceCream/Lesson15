313acc02bb05f71f88f859e5802221303fb09b08
FrenchIceCream <actually.star@gmail.com>
2021-10-26 12:35:08 +0300

    Реализована IsLeapYear

---
+++ Lesson15/DateTime.h
@@ -0,0 +1,15 @@
+#pragma once
+
+bool IsLeapYear(int year)
+{
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 != 0)
+				return false;
+		}
+		return true;
+	}
+	return false;
+}
